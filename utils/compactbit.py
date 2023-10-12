222d987b34adb6044ab41c52621ed52da7f7f8f5
1113609151 <1113609151@qq.com>
2023-10-12 15:30:23 +0800

    add compactbit

--- utils/compactbit.py
+++ utils/compactbit.py
@@ -1,34 +1,51 @@
 import numpy as np
 
 
-def compactbit(b):
-	'''
-	b = bits array
-	cb = compacted string of bits(using words of 'word' bits)
-	'''
-	b_mat = np.mat(b)
-	[nSamples, nbits] = b_mat.shape
-	nwords = int(np.ceil((float(nbits) / 8)))
-	cb = np.zeros((nSamples,nwords),dtype = np.uint8)
+import numpy as np
 
-	for i in xrange(nSamples):
-		for j in xrange(nwords):
-			temp = b[i , j * 8 : (j + 1) * 8]
-			value = convert(temp)
-			cb[i,j] = value
+def compactbit(binary_matrix):
+    """
+    Converts a binary matrix into a compact bit representation.
 
-	return cb
+    Parameters:
+    - binary_matrix (ndarray): The binary matrix to be converted.
+
+    Returns:
+    - compact_bit (ndarray): The compact bit representation of the binary matrix.
+    """
+    num_samples, num_bits = binary_matrix.shape
+    num_words = int(np.ceil(num_bits / 8))
+    compact_bit = np.zeros((num_samples, num_words), dtype=np.uint8)
+
+    for i in range(num_samples):
+        for j in range(num_words):
+            temp = binary_matrix[i, j * 8 : (j + 1) * 8]
+            value = convert(temp)
+            compact_bit[i, j] = value
+
+    return compact_bit
 
 def convert(arr):
-	arr_mat = np.mat(arr)
-	[_, col] = arr_mat.shape
-	value = 0
-	for i in xrange(col):
-		value = value + (2 ** i) * arr[i]
-	
-	return value
+    """
+    Converts an array of binary digits to an integer value.
+
+    Parameters:
+    - arr (List[int]): The array of binary digits to convert.
+
+    Returns:
+    - int: The integer value converted from the binary array.
+    """
+    value = 0
+    for i, digit in enumerate(arr):
+        value += (2 ** i) * digit
+    
+    return value
 
 
 if __name__ == '__main__':
-	b = np.array([[0,0,1,1,0,0,0,1,1],[0,0,0,1,0,0,1,1,1]])
-	print compactbit(b)
+	b = np.array([[0,0,1,1,0,0,0,1,1,1],[0,0,0,1,0,0,1,1,1,1]])
+	#测试convert
+	# print(convert([1,0,0]))
+
+	#测试compactbit
+	print(compactbit(b))
