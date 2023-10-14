9305571303025896e51dd0e5e239fbc5fb6d6e8c
1113609151 <1113609151@qq.com>
2023-10-14 15:41:18 +0800

    update model

--- model.py
+++ model.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn.functional as F
 from torch.nn.modules.utils import _pair
 import torch.nn.init as init
+from config import *
 
 class LocallyConnected2d(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride, bias=False):
@@ -19,11 +20,11 @@ class LocallyConnected2d(nn.Module):
         output_size = (x.shape[2] - self.kernel_size_1) // self.stride_1 + 1
         output_size = _pair(output_size)
         self.weight = nn.Parameter(
-            torch.randn(1, self.out_channels, self.in_channels, output_size[0], output_size[1], self.kernel_size_1**2)
+            torch.randn(1, self.out_channels, self.in_channels, output_size[0], output_size[1], self.kernel_size_1**2).cuda()
         )
         if self.bias:
             self.bias = nn.Parameter(
-                torch.randn(1, self.out_channels, output_size[0], output_size[1])
+                torch.randn(1, self.out_channels, output_size[0], output_size[1]).cuda()
             )
         else:
             self.register_parameter('bias', None)
@@ -34,6 +35,7 @@ class LocallyConnected2d(nn.Module):
         x = x.unfold(2, kh, dh).unfold(3, kw, dw)
         x = x.contiguous().view(*x.size()[:-2], -1)
         # Sum in in_channel and kernel_size dims
+        # print(self.weight.is_cuda)
         out = (x.unsqueeze(1) * self.weight).sum([2, -1])
         if self.bias is not None:
             out += self.bias
@@ -69,7 +71,9 @@ class DDH(nn.Module):
         self.C3_flatten = nn.Flatten()
 
         self.C4_block = nn.Sequential(
-            LocallyConnected2d(60, 80, kernel_size=2, stride=1, bias=True),
+            #nn.Conv2dLocal from https://github.com/pytorch/pytorch/pull/1583/files
+            nn.Conv2dLocal(in_channels=60, out_channels=80, in_height=3, in_width=3, kernel_size=2, stride=1, padding=0),
+            # LocallyConnected2d(60, 80, kernel_size=2, stride=1, bias=True),
             nn.BatchNorm2d(80),
             nn.ReLU(),
         )
@@ -90,31 +94,36 @@ class DDH(nn.Module):
             nn.Tanh()
         )
 
+        self.liner_last = nn.Linear(self.hash_num, NB_CLASSES)
+
     def forward(self, x):
         x = self.C1_block(x)
         x = self.C2_block(x)
         x = self.C3_block(x)
         x_1 = self.C3_flatten(x)
+        # print(x.shape)
         x = self.C4_block(x)
         x_2 = self.C4_flatten(x)
 
         x = torch.cat((x_1, x_2), dim=1)
-        x = nn.Linear(x.shape[1], self.hash_num * self.split_num)(x)
+        x = nn.Linear(x.shape[1], self.hash_num * self.split_num).cuda()(x)
         x = self.C5_block(x)
 
         outs = []
         for i in range(self.hash_num):
             slice_array = x[:, i * self.split_num : (i + 1) * self.split_num]
-            fuse_layer = nn.Linear(slice_array.shape[1], 1)
+            fuse_layer = nn.Linear(slice_array.shape[1], 1).cuda()
             outs.append(fuse_layer(slice_array))
 
         x = torch.cat(outs, dim=1)
+        x = self.C6_block(x)
 
-        return x
+        softmax = F.softmax(self.liner_last(x), dim=1)
+        return x, softmax
     
-
 if __name__ == "__main__":
     input_data = torch.randn(2, 3, 32, 32)
     model = DDH(10, 3, 3)
-    output = model(input_data)
-    print(output)
+    output, softmax = model(input_data)
+    print(output.shape)
+    print(softmax.shape)
